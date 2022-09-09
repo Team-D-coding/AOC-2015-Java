7dc8fe341757ee059db836c9c29eae2f19f19756
Nicqx <miklos.d.parrag@gmail.com>
2022-09-09 14:33:53 +0200

    day1-test

--- src/main/java/utility/InputReader.java
+++ src/main/java/utility/InputReader.java
@@ -2,6 +2,7 @@ package utility;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputReader {
--- test/Day1Test.java
+++ test/Day1Test.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import utility.InputReader;
 
-import static Days.Day1.processInput;
+import static Days.Day1.*;
 
 @DisplayName("Testing the Day1 class")
 class Day1Test {
@@ -21,20 +21,25 @@ class Day1Test {
 //        day1 = new Day1();
     }
 
-    @Test
     @ParameterizedTest
-    @ValueSource(strings = {"input1:0", "input2:0", "input3:3", "input4:3", "input5:3", "input6:-1", "input7:-1", "input8:-3", "input9:-3"})
+    @ValueSource(strings = {"input1:3", "input2:0", "input3:0", "input4:3", "input5:3", "input6:-1", "input7:-1", "input8:-3", "input9:-3"})
     void checkFirstPart(String files) {
         String inputFile = "test-resources/D1/" + files.split(":")[0];
-        String text = inputReader.fileReader(inputFile);
-        Assertions.assertEquals(processInput(text).get(Day1Enums.END_FLOOR), Integer.parseInt(files.split(":")[1]), "Expected result is not correct");
+        Assertions.assertEquals(processInput(inputFile).get(Day1Enums.END_FLOOR), Integer.parseInt(files.split(":")[1]), "Expected result is not correct");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"input10:1", "input11:5"})
     void checkSecondPart(String files) {
         String inputFile = "test-resources/D1/" + files.split(":")[0];
-        String text = inputReader.fileReader(inputFile);
-        Assertions.assertEquals(processInput(text).get(Day1Enums.BASEMENT_FIRST), Integer.parseInt(files.split(":")[1]), "Expected result is not correct");
+        Assertions.assertEquals(processInput(inputFile).get(Day1Enums.BASEMENT_FIRST), Integer.parseInt(files.split(":")[1]), "Expected result is not correct");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"input:74:1795"})
+    void checkOriginalInput(String files) {
+        String inputFile = "resources/D1/" + files.split(":")[0];
+        Assertions.assertEquals(processInput(inputFile).get(Day1Enums.END_FLOOR), Integer.parseInt(files.split(":")[1]), "Expected result is not correct");
+        Assertions.assertEquals(processInput(inputFile).get(Day1Enums.BASEMENT_FIRST), Integer.parseInt(files.split(":")[2]), "Expected result is not correct");
     }
 }
